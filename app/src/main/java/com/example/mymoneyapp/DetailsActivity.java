7272056db4283a8e5abde84006599f2fb1508f0f
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2020-05-13 23:22:38 +0800

    完成了修改数据的显示，下面要进行往数据库中进行写入

--- app/src/main/java/com/example/mymoneyapp/DetailsActivity.java
+++ app/src/main/java/com/example/mymoneyapp/DetailsActivity.java
@@ -4,32 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.Data;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.text.BreakIterator;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 //这是关于账单信息的
@@ -45,6 +38,10 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     private TextView tvshouru;
     private ListView listView;
     private static List<GetData>dataList=new ArrayList<GetData>();
+    public List<GetData>arrResult=new ArrayList<GetData>();
+    //信息修改部分
+    public static GetData result;
+    private EditText idText=null, leixingText=null, xiangmuText =null, jineText =null, beizhuText =null, riqiText =null;
     @Override
     protected void onStart() {
         super.onStart();updateBill();
@@ -64,10 +61,17 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         tvzhichu=findViewById(R.id.showzhichu);
 
         listView=findViewById(R.id.datashow);
+        LayoutInflater layoutInflater = LayoutInflater.from(DetailsActivity.this);
+        View layout=layoutInflater.inflate(R.layout.activity_update,null);
+        //数据修改部分
+        idText =layout.findViewById(R.id.editId);
+        leixingText =layout.findViewById(R.id.editLeixing);
+        xiangmuText =layout.findViewById(R.id.editxiangmu);
+        jineText =layout.findViewById(R.id.editJine);
+        beizhuText =layout.findViewById(R.id.editBeizhu);
+        riqiText =layout.findViewById(R.id.editDate);
         //ArrayAdapter<GetData>arrayAdapter=new ArrayAdapter<GetData>(this,R.layout.data_line,dataList);
         //listView.setAdapter(arrayAdapter);
-
-
         //底部导航栏
         final BottomNavigationView bottomNavigationView=findViewById(R.id.nav_view);
         bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
@@ -102,8 +106,27 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         //数据库查询结果的显示
         inquire();
         updateBill();
+
         System.out.println("主"+dataList);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent1=new Intent(DetailsActivity.this,UpdateActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                ArrayAdapter<GetData> adapter = new ArrayAdapter<GetData>(
+                        DetailsActivity.this,   // Context上下文
+                        android.R.layout.simple_list_item_1,  // 子项布局id
+                        dataList);
+                result=adapter.getItem(position);
+                //System.out.println(result.);
+                //idText.setText(result.getId());leixingText.setText(result.getLeixing());xiangmuText.setText(result.getXiangmu());
+                //jineText.setText(""+result.getMoney());beizhuText.setText(result.getBeizhu());riqiText.setText(""+result.getDate());
+                startActivity(intent1);
+                System.out.println(leixingText.getText().toString());
+            }
+        });
     }
+
     /*
     *更新账单
     */
--- app/src/main/java/com/example/mymoneyapp/UpdateActivity.java
+++ app/src/main/java/com/example/mymoneyapp/UpdateActivity.java
@@ -2,14 +2,47 @@ package com.example.mymoneyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class UpdateActivity extends AppCompatActivity {
+    private LinearLayout llreturn=null;
+    private EditText idText=null, leixingText=null, xiangmuText =null, jineText =null, beizhuText =null, riqiText =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+        //
+        idText =findViewById(R.id.editId);
+        leixingText =findViewById(R.id.editLeixing);
+        xiangmuText =findViewById(R.id.editxiangmu);
+        jineText =findViewById(R.id.editJine);
+        beizhuText =findViewById(R.id.editBeizhu);
+        riqiText =findViewById(R.id.editDate);
+
+        idText.setText(DetailsActivity.result.getId());leixingText.setText(DetailsActivity.result.getLeixing());xiangmuText.setText(DetailsActivity.result.getXiangmu());
+        jineText.setText(""+DetailsActivity.result.getMoney());beizhuText.setText(DetailsActivity.result.getBeizhu());riqiText.setText(""+DetailsActivity.result.getDate());
+        //返回按键
+        llreturn=findViewById(R.id.upreturn);
+        llreturn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent1=new Intent(UpdateActivity.this,DetailsActivity.class);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent1);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
     }
 }
